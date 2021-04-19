d51bbb8223ef6c57ec982ed05d641087e1a4d45b
jcrawfo4 <jkbcrawford@gmail.com>
2021-04-19 02:06:02 -0500

    final submission at 2 am

--- CLJ_app/urls.py
+++ CLJ_app/urls.py
@@ -6,18 +6,18 @@ from . import views
 urlpatterns = [
     #categories URLs
     path('', views.categories, name='categories'),
-    path('<int:category_id>', views.category_details, name='category_detail'),
-    path('new', views.new_category, name='new_category'),
-    path('<int:category_id>/edit', views.category_edit, name='category_edit'),
-    path('<int:category_id>/delete', views.category_delete, name='category_delete'),
+    path('<int:category_id>/', views.category_details, name='category_detail'),
+    path('new/', views.new_category, name='new_category'),
+    path('<int:category_id>/edit/', views.category_edit, name='category_edit'),
+    path('<int:category_id>/delete/', views.category_delete, name='category_delete'),
 
 
     # posts URL
     path('posts', views.posts, name='posts'),
-    path('<int:category_id>/posts/<int:post_id>', views.post_detail, name='post_detail'),
-    path('<int:category_id>/posts/new', views.new_post, name='new_post'),
-    path('<int:category_id>/posts/<int:post_id>/edit', views.post_edit, name='post_edit'), 
-    path('<int:category_id>/posts/<int:post_id>/delete', views.post_delete, name='post_delete')
+    path('<int:category_id>/posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('<int:category_id>/posts/new/', views.new_post, name='new_post'),
+    path('<int:category_id>/posts/<int:post_id>/edit/', views.post_edit, name='post_edit'), 
+    path('<int:category_id>/posts/<int:post_id>/delete/', views.post_delete, name='post_delete')
 
 ]
 
--- CLJ_app/views.py
+++ CLJ_app/views.py
@@ -24,9 +24,9 @@ def new_category(request):
             category.save()
             return redirect('category_detail', 
             category_id = category.id)
-        else:
-            form = CategoryForm()
-        return render(request, 'templates/category_form.html', {'form':form, 'type':'New'})
+    else:
+        form = CategoryForm()
+    return render(request, 'category_form.html', {'form':form, 'type':'New'})
 
 def category_edit(request, category_id):
     category = Category.objects.get(id=category_id)
@@ -46,22 +46,26 @@ def category_delete(request, category_id):
     if request== 'POST':
         category = Category.objects.get(id=category_id)
         category.delete()
-    return redirect('category_list')
+    return redirect('categories')
 
 ##### posts
 def post_detail(request, category_id, post_id):
     category = Category.objects.get(id = category_id)
     post = Post.objects.get(id=post_id)
-    return render(request, 'post_detail.html', {'category': category, 'posts': post})
+    return render(request, 'post_detail.html', {'category': category, 'post': post})
 
 def new_post(request, category_id):
     category = get_category(category_id)
-    if request== 'POST':
+    if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
+            print("this happened !!!!")
             post = form.save(commit=False)
             post.save()
             return redirect('post_detail', category_id=category.id, post_id=post.id)
+        else: 
+            print(form.errors)
+            return render(request, 'post_form.html', {'form':form, 'type':'New', 'category':category})
     else:
         form = PostForm(initial={'category':category})
     return render(request, 'post_form.html', {'form':form, 'type':'New', 'category':category})
@@ -82,7 +86,7 @@ def post_edit(request, category_id, post_id):
 
 def post_delete(request, category_id, post_id):
     if request.method == "POST":
-        post = post.objects.get(id=post_id)
+        post = Post.objects.get(id=post_id)
         post.delete()
     return redirect('category_detail', category_id=category_id)
     
